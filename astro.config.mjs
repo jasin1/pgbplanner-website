4cb0c60865fb50013e52f6ea0df26b0891674b42
jasin1 <jasin.tairaidrissi@gmail.com>
2026-07-22 15:04:45 +0200

    feat: prerender blog routes and generate sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://www.pgbplanner.nl',
   output: 'server',
   adapter: netlify(),
+  integrations: [sitemap()],
 });
